08f091dc7a3e16d4e833b1f9203dad4212eb850f
Daryl Bacurin <darylbacurin14@gmail.com>
2024-07-30 23:09:02 +0800

    Main Update

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,10 +2,11 @@ import CallToActionSection from "./CallToActionSection/CallToActionSection"
 import IntroSection from "./IntroSection/IntroSection"
 import ShortenSection from "./ShortenSection/ShortenSection"
 import StatisticsSection from "./StatisticsSection/StatisticsSection"
+import styles from './main.module.scss';
 
 export default function Main(){
     return(
-        <main>
+        <main className={styles['b-main']}>
             <IntroSection/>
             <ShortenSection/>
             <StatisticsSection/>
