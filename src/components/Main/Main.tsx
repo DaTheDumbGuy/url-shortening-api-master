c54f34a8807b264a8be23406787f418f2943b902
Daryl Bacurin <darylbacurin14@gmail.com>
2024-08-04 22:19:09 +0800

    Update Tablet

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,11 +2,10 @@ import CallToActionSection from "./CallToActionSection/CallToActionSection"
 import IntroSection from "./IntroSection/IntroSection"
 import ShortenSection from "./ShortenSection/ShortenSection"
 import StatisticsSection from "./StatisticsSection/StatisticsSection"
-import styles from './main.module.scss';
 
 export default function Main(){
     return(
-    <main className={styles['b-main']}>
+    <main>
         <IntroSection/>
         <ShortenSection/>
         <StatisticsSection/>
